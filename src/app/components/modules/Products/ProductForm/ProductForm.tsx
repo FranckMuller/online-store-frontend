e47af2b24672411a9a02262eb74fabfdc1d46297
FranckMuller <dmitrysvetlov113@gmail.com>
2023-10-28 17:59:33 +0300

    published product

--- src/app/components/modules/Products/ProductForm/ProductForm.tsx
+++ src/app/components/modules/Products/ProductForm/ProductForm.tsx
@@ -28,21 +28,26 @@ const ProductForm = () => {
     error,
     data,
   } = useMutation({
-    mutationFn: (productData: FormData) => Api.products.create(productData)
-    
+    mutationFn: (productData: FormData) => Api.products.create(productData),
   });
 
   const [formData, setFormData] = useState<ProductFormData>(initialData);
+  const [published, setPublished] = useState(true);
   const [images, setImages] = useState<FileList | null>(null);
 
   const onChangeInput = (
     e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const target = e.currentTarget ?? e.target;
+    console.log(target);
     setFormData((prev) => ({
       ...prev,
       [target.name]: target.value,
     }));
+  };
+
+  const onChangePublished = () => {
+    setPublished((prev) => !prev);
   };
 
   const onChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
@@ -60,6 +65,7 @@ const ProductForm = () => {
         data.append("name", formData.name);
         data.append("description", formData.description);
         data.append("price", formData.price);
+        data.append("published", published);
         for (let i = 0; i < images.length; i++) {
           data.append("images", images[i]);
         }
@@ -128,6 +134,17 @@ const ProductForm = () => {
           />
         </div>
 
+        <div className={styles["form-group__checkbox"]}>
+          <label htmlFor="published">Publish:</label>
+          <input
+            className={styles["form-control"]}
+            id="published"
+            name="published"
+            checked={published}
+            onChange={onChangePublished}
+            type="checkbox"
+          />
+        </div>
         <div className={styles["submit-button-wrapper"]}>
           <button className={styles["submit-button"]}>create product</button>
         </div>
